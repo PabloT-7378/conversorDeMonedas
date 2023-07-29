c68c2b29e27cf3686f5e1c46bb1fc5c6b6dd6f34
PabloT-7378 <nellyantonio7378@gmail.com>
2023-07-29 17:49:58 -0300

    Algunos Arreglos

--- ConversorDeMonedas/src/ar/com/conversorDeMonedas/codigo/ConversorDeMoneda.java
+++ ConversorDeMonedas/src/ar/com/conversorDeMonedas/codigo/ConversorDeMoneda.java
@@ -4,83 +4,70 @@ import javax.swing.JOptionPane;
 
 public class ConversorDeMoneda {
 	
-	public static void main(String[] args) {
-		
-		mostrarMenuPrincipal();
-		
-	}
-	
-	public static void mostrarMenuPrincipal() {
-		
-		String[] opciones = {
-				"Conversor de Moneda",
-				"Conversor de Peso",
-				"Salir"
-		};
-		
-		int seleccion = JOptionPane.showOptionDialog(
-				null,
-				"Seleccione una Opcion: ",
-				"Menu Conversor",
-				JOptionPane.DEFAULT_OPTION,
-				JOptionPane.PLAIN_MESSAGE,
-				null,
-				opciones,
-				opciones[0]
-		);
-		
-		switch (seleccion) {
-				case 0:
-					realizarConversionMoneda();
-					break;
-				case 1:
-					realizarConversionPeso();
-					break;
-				case 2:
-					JOptionPane.showMessageDialog(	null, "Gracias por su visita!");
-					System.exit(0);
-					break;
-				default:
-					JOptionPane.showMessageDialog(null, "Opcion Invalida, Saliendo del Programa.");
-					System.exit(1);
-					break;
+		public static void main(String[] args) {
+			
+			mostrarMenuPrincipal();
+			
 		}
+	
+		public static void mostrarMenuPrincipal() {
+			
+			String[] opciones = {
+					"Conversor de Moneda",
+					"Conversor de Peso",
+					"Salir"
+					
+			};
 		
-	}
+			String opcionSeleccionada = (String) JOptionPane.showInputDialog(
+					
+		            null,
+		            "Seleccione una Opción:",
+		            "Menu Conversor",
+		            JOptionPane.PLAIN_MESSAGE,
+		            null,
+		            opciones,
+		            opciones[0]
+		            		
+		     );
+
+			if (opcionSeleccionada == null) {
+	            
+	            JOptionPane.showMessageDialog(null, "Gracias por su visita!");
+	            System.exit(0);
+	            
+	        } else {
+            
+		        switch (opcionSeleccionada) {
+	                case "Conversor de Moneda":
+	                    realizarConversionMoneda();
+	                    break;
+	                case "Conversor de Peso":
+	                    realizarConversionPeso();
+	                    break;
+	                case "Salir":
+	                    JOptionPane.showMessageDialog(null, "Gracias por su visita!");
+	                    System.exit(0);
+	                    break;
+	                default:
+	                    JOptionPane.showMessageDialog(null, "Opción Inválida.");
+	                    mostrarMenuPrincipal();
+	                    break;
+	                    
+	            }
+        }
+ }
+
 	
 	public static void realizarConversionMoneda() {
-		String cantidadStr = JOptionPane.showInputDialog(null, "Ingrese la cantidad de dinero:");
-		
-		if (cantidadStr == null) {
-			// Si el usuario presiona "Cancelar", volvemos al menú principal
-			mostrarMenuPrincipal();
-			return;
-		}
-		
-		try {
-			// Convertir la cantidad ingresada a un valor numérico
-			double cantidad = Double.parseDouble(cantidadStr);
+        // Aquí implementarías el código para el conversor de moneda
+        // Puedes utilizar JOptionPane.showInputDialog para pedir al usuario los datos necesarios
+        // y mostrar el resultado de la conversión en un JOptionPane.showMessageDialog
+        // También puedes utilizar APIs de conversión de moneda, como por ejemplo https://exchangeratesapi.io/
 
-			// Aquí implementarías la lógica de conversión de moneda
-			// Puedes pedir al usuario que ingrese la moneda de origen y destino utilizando
-			// JOptionPane.showInputDialog y mostrar el resultado de la conversión en un
-			// JOptionPane.showMessageDialog
-			// También puedes utilizar APIs de conversión de moneda, como por ejemplo https://exchangeratesapi.io/
-
-			// Supongamos que la conversión devuelve un valor convertido
-			double resultado = cantidad * 1.12; // Conversión de dólares a euros (valor ficticio)
-
-			// Mostrar el resultado en un cuadro de diálogo
-			JOptionPane.showMessageDialog(null, cantidad + " dólares son " + resultado + " euros.");
-
-		} catch (NumberFormatException e) {
-			// Si el usuario ingresó un valor no numérico, mostrar un mensaje de error
-			JOptionPane.showMessageDialog(null, "Cantidad inválida. Por favor, ingrese un valor numérico.");
-		}
-
-		// Volver al menú principal después de mostrar el resultado o el mensaje de error
-		mostrarMenuPrincipal();
-	}
+        JOptionPane.showMessageDialog(null, "Función de Conversor de Moneda aún no implementada.");
+        mostrarMenuPrincipal();
+    }
 	
 	 public static void realizarConversionPeso() {
 	        // Implementar aquí la lógica de la otra función 1
